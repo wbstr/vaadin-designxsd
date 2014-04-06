01272811f4cc57f0d8cfc37807b6ef98eaee92c0
kumm <kumm@webstar.hu>
2014-04-06 19:04:42 +0200

    license

--- src/test/java/com/wcs/maven/claraxsd/attributebuilder/AttributeBuilderFactoryTest.java
+++ src/test/java/com/wcs/maven/claraxsd/attributebuilder/AttributeBuilderFactoryTest.java
@@ -1,19 +1,28 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2014 Webstar Csoport Kft.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package com.wcs.maven.claraxsd.attributebuilder;
 
-import com.wcs.maven.claraxsd.attributebuilder.AttributeBuilderFactory;
-import com.wcs.maven.claraxsd.attributebuilder.AttributeBuilder;
-import com.wcs.maven.claraxsd.attributebuilder.NopAttributeBuilder;
-import java.util.Arrays;
 import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaAttribute;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  *
