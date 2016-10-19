f7e94526d71f52a9b059f85c8153ef1031bb6554
Lali <balazs.lajos@webstar.hu>
2016-10-19 10:51:13 +0200

    move

--- designxsd-maven-plugin/src/test/java/com/wcs/maven/designxsd/discoverer/AttributeDiscovererTest.java
+++ designxsd-maven-plugin/src/test/java/com/wcs/maven/designxsd/discoverer/AttributeDiscovererTest.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.wcs.maven.designxsd;
+package com.wcs.maven.designxsd.discoverer;
 
-import com.wcs.maven.designxsd.discoverer.AttributeDiscoverer;
 import com.vaadin.ui.ColorPicker;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Flash;
